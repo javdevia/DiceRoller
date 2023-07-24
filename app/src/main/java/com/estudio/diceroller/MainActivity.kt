590e04fdde18e0d5b8cf257df791492ae0e703e1
Javier <javdevia@gmail.com>
2023-07-24 17:15:57 -0400

    Se agrega resultados con recursos de imagen

--- app/src/main/java/com/estudio/diceroller/MainActivity.kt
+++ app/src/main/java/com/estudio/diceroller/MainActivity.kt
@@ -3,14 +3,16 @@ package com.estudio.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import javax.crypto.spec.IvParameterSpec
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnRoll: Button
-    lateinit var tvResult: TextView
+    lateinit var ivDiceRoll: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +23,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun InitUI() {
         btnRoll = findViewById(R.id.btnRoll)
-        tvResult = findViewById(R.id.tvResult)
+        ivDiceRoll = findViewById(R.id.ivDiceRoll)
+        ivDiceRoll.setImageResource(R.drawable.dice_1)
     }
 
     private fun InitListeners() {
@@ -30,7 +33,9 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
 
             val firstDice = Dice(6)
-            tvResult.text = firstDice.roll().toString()
+            val result: Int = firstDice.roll()
+
+            printDice(result)
         }
     }
 
@@ -38,5 +43,20 @@ class MainActivity : AppCompatActivity() {
         fun roll(): Int {
             return (1..numSides).random()
         }
+    }
+
+    private fun printDice(roll: Int) {
+
+        val drawableResourse = when (roll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_1
+        }
+
+        return ivDiceRoll.setImageResource(drawableResourse)
     }
 }
