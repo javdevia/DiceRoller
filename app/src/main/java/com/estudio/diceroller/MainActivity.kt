7bdf7b5161fa7fb664b30f9b66e3b9dfcacf062b
Javier <javdevia@gmail.com>
2023-07-24 16:46:05 -0400

    Funcion de lanzar dado funcionando de forma basica

--- app/src/main/java/com/estudio/diceroller/MainActivity.kt
+++ app/src/main/java/com/estudio/diceroller/MainActivity.kt
@@ -2,10 +2,41 @@ package com.estudio.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnRoll: Button
+    lateinit var tvResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        InitUI()
+        InitListeners()
+    }
+
+    private fun InitUI() {
+        btnRoll = findViewById(R.id.btnRoll)
+        tvResult = findViewById(R.id.tvResult)
+    }
+
+    private fun InitListeners() {
+        btnRoll.setOnClickListener {
+
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+
+            val firstDice = Dice(6)
+            tvResult.text = firstDice.roll().toString()
+        }
+    }
+
+    private class Dice(val numSides: Int) {
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
     }
 }
